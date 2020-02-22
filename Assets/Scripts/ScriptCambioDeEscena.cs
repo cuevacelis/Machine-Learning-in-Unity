7c90d0b33672822489a34579df568322953d74f5
Jose Cueva Celis <cuevacelishotmail.com>
2020-02-22 11:17:51 -0500

    Subiendo por primera vez
    Esta es la primera subida al Repositiorio de Git.
    Se crearon las interfazes iniciales del proyecto

---
+++ Assets/Scripts/ScriptCambioDeEscena.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScriptCambioDeEscena : MonoBehaviour
+{
+    public void CambioDeEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    /*
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
